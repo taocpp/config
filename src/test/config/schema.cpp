3ef61fb47e3418292e8e528c29d3142af62b357f
Daniel Frey <d.frey@gmx.de>
2019-04-28 12:12:08 +0200

    Initial schema validation

---
+++ src/test/config/schema.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/config/
+
+#include <tao/config.hpp>
+
+int main()
+{
+   const auto tcs = tao::config::schema::read( "tests/schema.tcs" );
+
+   const auto data = tao::json::jaxn::basic_parse_file< tao::config::traits >( "tests/schema.jaxn" );
+   assert( tcs.validate( data ) );
+
+   return 0;
+}
