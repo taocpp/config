08fb0b01aac6736914fe0b608c8d4b89679559bc
Dr. Colin Hirsch <github@colin-hirsch.net>
2019-07-23 15:10:34 +0200

    Improve messages.

--- include/tao/config/internal/phase2_access.hpp
+++ include/tao/config/internal/phase2_access.hpp
@@ -12,6 +12,7 @@
 #include "entry.hpp"
 #include "format.hpp"
 #include "forward.hpp"
+#include "pegtl.hpp"
 #include "reverse.hpp"
 #include "state.hpp"
 
@@ -19,48 +20,48 @@ namespace tao::config::internal
 {
    struct temporary_non_local_return_hack {};  // TODO: Ensure this doesn't survive the first wave of refactoring after stabilising the semantics.
 
-   inline const json_t& phase2_access_impl( const concat& l, const key& p );
+   inline const json_t& phase2_access_impl( const pegtl::position& pos, const concat& l, const key& p );
 
-   inline const json_t& phase2_access_impl( const entry_array& a, const part& t, const key& p )
+   inline const json_t& phase2_access_impl( const pegtl::position& pos, const entry_array& a, const part& t, const key& p )
    {
       switch( t.type() ) {
          case part::star:
-            throw std::runtime_error( "attempt to reference star" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to index array with star", { { "array", &a.position() } } ), pos );
          case part::minus:
             if( !a.empty() ) {
-               return phase2_access_impl( a.back(), p );
+               return phase2_access_impl( pos, a.back(), p );
             }
-            throw std::runtime_error( "attempt to reference last entry of empty array" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to reference last entry of empty array", { { "array", &a.position() } } ), pos );
          case part::name:
-            throw std::runtime_error( "attempt to reference string in array" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to reference string in array", { { "array", &a.position() }, { "string", t.get_name() } } ), pos );
          case part::index:
             if( a.size() > t.get_index() ) {
-               return phase2_access_impl( a[ t.get_index() ], p );
+               return phase2_access_impl( pos, a[ t.get_index() ], p );
             }
-            throw std::runtime_error( "array reference out of bounds" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "array reference out of bounds", { { "array", &a.position() }, { "integer", t.get_index() }, { "size", a.size() } } ), pos );
       }
       assert( false );
    }
 
-   inline const json_t& phase2_access_impl( const entry_object& o, const part& t, const key& p )
+   inline const json_t& phase2_access_impl( const pegtl::position& pos, const entry_object& o, const part& t, const key& p )
    {
       switch( t.type() ) {
          case part::star:
-            throw std::runtime_error( "attempt to reference star" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to index object with star", { { "object", &o.position() } } ), pos );
          case part::minus:
-            throw std::runtime_error( "attempt to reference last entry object" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to reference last entry in object", { { "object", &o.position() } } ), pos );
          case part::name:
             if( const auto* i = o.find( t.get_name() ) ) {
-               return phase2_access_impl( i->second, p );
+               return phase2_access_impl( pos, i->second, p );
             }
-            throw std::runtime_error( "object reference not found" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "object reference not found", { { "object", &o.position() }, { "string", t.get_name() } } ), pos );
          case part::index:
-            throw std::runtime_error( "attempt to reference index in object" );
+            throw pegtl::parse_error( format( __FILE__, __LINE__, "attempt to reference integer in object", { { "object", &o.position() }, { "integer", t.get_index() } } ), pos );
       }
       assert( false );
    }
 
-   inline const json_t& phase2_access_impl( const concat& l, const key& p )
+   inline const json_t& phase2_access_impl( const pegtl::position& pos, const concat& l, const key& p )
    {
       if( l.size() == 1 ) {
          auto& e = l.entries().front();
@@ -72,12 +73,12 @@ namespace tao::config::internal
                return config::access( e.get_atom(), p );
             case entry::array:
                if( !p.empty() ) {
-                  return phase2_access_impl( e.get_array(), p.front(), pop_front( p ) );
+                  return phase2_access_impl( pos, e.get_array(), p.front(), pop_front( p ) );
                }
                break;
             case entry::object:
                if( !p.empty() ) {
-                  return phase2_access_impl( e.get_object(), p.front(), pop_front( p ) );
+                  return phase2_access_impl( pos, e.get_object(), p.front(), pop_front( p ) );
                }
                break;
             case entry::reference:
@@ -89,10 +90,10 @@ namespace tao::config::internal
       throw temporary_non_local_return_hack();  // Unsuitable thing found -- abort search and try again next iteration.
    }
 
-   inline const json_t* phase2_access( const entry_object& o, const std::string& k, const key& p )
+   inline const json_t* phase2_access( const pegtl::position& pos, const entry_object& o, const std::string& k, const key& p )
    {
       if( const auto* i = o.find( k ) ) {
-         return &phase2_access_impl( i->second, p );  // No backtracking once we enter _impl.
+         return &phase2_access_impl( pos, i->second, p );  // No backtracking once we enter _impl.
       }
       return nullptr;  // Nothing found -- backtrack search in entry/concat tree.
    }
@@ -104,11 +105,11 @@ namespace tao::config::internal
       for( const auto& i : reverse( l.entries() ) ) {
          switch( i.type() ) {
             case entry::atom:
-               throw pegtl::parse_error( "addition of atom and object detected", pos );
+               throw pegtl::parse_error( "addition of atom and object detected", l.p );
             case entry::array:
-               throw pegtl::parse_error( "addition of array and object detected", pos );
+               throw pegtl::parse_error( "addition of array and object detected", l.p );
             case entry::object:
-               if( const json_t* c = phase2_access( i.get_object(), k, p ) ) {
+               if( const json_t* c = phase2_access( pos, i.get_object(), k, p ) ) {
                   return c;
                }
                break;
@@ -132,7 +133,7 @@ namespace tao::config::internal
             if( e.parent() ) {
                return phase2_access( pos, *e.parent(), k, p );
             }
-            if( const json_t* c = phase2_access( e.get_object(), k, p ) ) {
+            if( const json_t* c = phase2_access( pos, e.get_object(), k, p ) ) {
                return c;
             }
             break;
@@ -161,12 +162,12 @@ namespace tao::config::internal
       assert( false );
    }
 
-   inline const json_t* phase2_access( const entry& e, const key& p )
+   inline const json_t* phase2_access( const pegtl::position& pos, const entry& e, const key& p )
    {
       assert( !p.empty() );
 
       try {
-         return phase2_access( e.position(), e, p.front(), pop_front( p ) );
+         return phase2_access( pos, e, p.front(), pop_front( p ) );
       }
       catch( const temporary_non_local_return_hack& ) {
          return nullptr;
