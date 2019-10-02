3342273193813a7dee0dbced051fd0277280c465
Daniel Frey <d.frey@gmx.de>
2019-10-02 22:20:54 +0200

    Refactor istring

---
+++ include/tao/config/schema/internal/iless.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2019 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/config/
+
+#ifndef TAO_CONFIG_SCHEMA_INTERNAL_ILESS_HPP
+#define TAO_CONFIG_SCHEMA_INTERNAL_ILESS_HPP
+
+#include <string_view>
+#include <strings.h>
+
+namespace tao::config::schema::internal
+{
+   struct iless
+   {
+      // TODO: Compare international strings (Unicode, using ICU)
+      bool operator()( const std::string_view lhs, const std::string_view rhs ) const noexcept
+      {
+         const auto d = ::strncasecmp( lhs.begin(), rhs.begin(), ( std::min )( lhs.size(), rhs.size() ) );
+         return ( d < 0 ) || ( ( d == 0 ) && ( lhs.size() < rhs.size() ) );
+      }
+   };
+
+}  // namespace tao::config::schema::internal
+
+#endif
--- include/tao/config/schema/internal/istring.hpp
+++ include/tao/config/schema/internal/istring.hpp
@@ -4,37 +4,38 @@
 #ifndef TAO_CONFIG_SCHEMA_INTERNAL_ISTRING_HPP
 #define TAO_CONFIG_SCHEMA_INTERNAL_ISTRING_HPP
 
-#include <strings.h>
-
+#include "iless.hpp"
 #include "node.hpp"
+
+#include <set>
+#include <string_view>
 
 namespace tao::config::schema::internal
 {
    struct istring : node
    {
-      std::vector< std::string_view > m_values;
+      std::set< std::string_view, iless > m_values;
 
       explicit istring( const value& v )
          : node( v )
       {
          if( m_source.is_array() ) {
             for( const auto& e : m_source.unsafe_get_array() ) {
-               m_values.emplace_back( e.as< std::string_view >() );
+               m_values.emplace( e.as< std::string_view >() );
             }
          }
          else {
-            m_values.emplace_back( m_source.as< std::string_view >() );
+            m_values.emplace( m_source.as< std::string_view >() );
          }
       }
 
       json::value validate( const value& v ) const override
       {
          const auto s = v.as< std::string_view >();
-         for( const auto sv : m_values ) {
-            if( ( s.size() == sv.size() ) && ( ::strncasecmp( s.begin(), sv.begin(), s.size() ) == 0 ) ) {
-               return ok();
-            }
+         if( m_values.count( s ) == 1 ) {
+            return ok();
          }
+
          json::value candidates = json::empty_array;
          for( const auto sv : m_values ) {
             candidates.unsafe_emplace_back( sv );
