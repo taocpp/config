e52e6093d44ca72773b20e8bb4cad92979ad63e1
Dr. Colin Hirsch <github@colin-hirsch.net>
2020-12-10 13:30:10 +0100

    WiP.

--- include/tao/config/internal/json.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018-2020 Dr. Colin Hirsch and Daniel Frey
-// Please see LICENSE for license or visit https://github.com/taocpp/config/
-
-#ifndef TAO_CONFIG_INTERNAL_JSON_HPP
-#define TAO_CONFIG_INTERNAL_JSON_HPP
-
-#include "../external/json.hpp"
-
-#endif
