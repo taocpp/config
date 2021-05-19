f4afc493fef2764917553ed62d91fa035b4c58be
Dr. Colin Hirsch <github@colin-hirsch.net>
2021-05-19 21:12:12 +0200

    Fix typo.

--- include/tao/config/internal/state.hpp
+++ include/tao/config/internal/state.hpp
@@ -30,7 +30,7 @@ namespace tao::config::internal
       ~state() = default;
 
       void operator=( state&& ) = delete;
-      void operator=( const state&& ) = delete;
+      void operator=( const state& ) = delete;
 
       key1 prefix;
       key1 suffix;
@@ -39,6 +39,9 @@ namespace tao::config::internal
       std::string schema;
 
       std::uint64_t generation = 1;
+
+      static constexpr std::size_t depth_limit = 96;
+      static constexpr std::size_t concat_limit = 64;
    };
 
 }  // namespace tao::config::internal
